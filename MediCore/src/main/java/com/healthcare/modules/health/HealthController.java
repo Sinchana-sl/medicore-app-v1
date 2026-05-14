37dc1a63678142cbf55b8d1ed8a24bd1d1f53717
Sinchana-sl <sinchanaslnaidu1610@gmail.com>
2026-05-14 10:25:03 +0530

    fix

--- MediCore/src/main/java/com/healthcare/modules/health/HealthController.java
+++ MediCore/src/main/java/com/healthcare/modules/health/HealthController.java
@@ -1,13 +1,14 @@
 package com.healthcare.modules.health;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
+    @RequestMapping(value = "/health", method = {RequestMethod.GET, RequestMethod.HEAD})
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
     }
